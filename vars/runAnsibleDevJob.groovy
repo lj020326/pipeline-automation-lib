218f8fc33c15ec09860bec52d1e2baa63d49d571
Lee Johnson <lee.james.johnson@gmail.com>
2022-03-30 17:48:22 -0400

    master - 	modified:   .DS_Store 	modified:   vars/runAnsibleDevJob.groovy

--- vars/runAnsibleDevJob.groovy
+++ vars/runAnsibleDevJob.groovy
@@ -43,6 +43,8 @@ def call(Map config=[:]) {
     config.ansiblePlaybook = 'site.yml'
     config.gitPerformCheckout = false
 
+    log.info("${logPrefix} env.BRANCH_NAME=${$env.BRANCH_NAME}")
+
     log.info("${logPrefix} config=${JsonUtils.printToJsonString(config)}")
 
     runAnsiblePlaybook(config)
