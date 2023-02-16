ed3591e2d3bb2527ce283193ac91d5f1bdc4207a
Lee Johnson <lee.james.johnson@gmail.com>
2023-02-16 09:33:30 -0500

    master - 	modified:   vars/buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -114,17 +114,11 @@ void buildAndPublishImage(Logger log, Map config) {
         stage("${stageName}") {
             // ref: https://www.jenkins.io/doc/book/pipeline/docker/
             if (config?.buildPath && config?.dockerFile) {
-//                 app = docker.build(config.buildImageLabel, "-f ${config.dockerFile} ${config.buildPath}")
                 buildArgs += "-f ${config.dockerFile} ${config.buildPath}"
             }
             else if (config?.dockerFile) {
-//                 app = docker.build(config.buildImageLabel, "-f ${config.dockerFile} .")
                 buildArgs += "-f ${config.dockerFile} ."
             }
-//             else if (config?.buildPath) {
-// //                 app = docker.build(config.buildImageLabel, config.buildPath)
-//                 buildArgs += config.buildPath
-//             }
             if (buildArgs) {
                 app = docker.build(config.buildImageLabel, buildArgs)
             else {
